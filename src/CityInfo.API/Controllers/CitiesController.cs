b23eaf9054011f20b7de3ba2f57532fe6dbac33d
Jannes Coetzee <jannesrsa@gmail.com>
2018-07-27 10:16:38 +0200

    Add CitiesController and Routing

---
+++ src/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    //[Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new {id=1,Name="NYC" },
+                new {id=2,Name="Cape Town" }
+            });
+        }
+    }
+}
