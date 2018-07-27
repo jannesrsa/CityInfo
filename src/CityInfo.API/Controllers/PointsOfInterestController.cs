d5f228a32687fdda6002098236ac558bb1c488a6
Jannes Coetzee <jannesrsa@gmail.com>
2018-07-27 11:04:10 +0200

    Add Child Resources.

---
+++ src/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    [ApiController]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult Get(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities
+                .FirstOrDefault(i => i.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        public IActionResult Get(int cityid, int id)
+        {
+            var pointOfInterest = CitiesDataStore.Current.Cities
+               .FirstOrDefault(i => i.Id == cityid)
+               ?.PointsOfInterest.FirstOrDefault(i => i.Id == id);
+
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+
+        // POST: api/PointsOfInterest
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/PointsOfInterest/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- src/CityInfo.API/Models/CityDto.cs
+++ src/CityInfo.API/Models/CityDto.cs
@@ -1,10 +1,13 @@
-﻿namespace CityInfo.API.Models
+﻿using System.Collections.Generic;
+
+namespace CityInfo.API.Models
 {
     public class CityDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string NumberOfPointsOfInterest { get; set; }
+        public int NumberOfPointsOfInterest => PointsOfInterest.Count;
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
     }
 }
